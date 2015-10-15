6ec469f855dfa6ea61ed1a52dd95a795bd5ebd11
Steven Young <fivepockets@gmail.com>
2015-10-15 12:01:28 -0700

    use jade for template enjoin

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
-var express = require('express');
-var app = express();
-var fs = require("fs");
+var fs = require("fs")
+var path = require('path')
+var express = require('express')
+var app = express()
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'jade')
 
 app.get('/listUsers', function (req, res) {
   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-    console.log( data );
-    res.end( data );
-  });
+    console.log(data)
+    res.end(data)
+  })
+})
+
+app.get('/home', function(req, res) {
+  res.render('index')
 })
 
 app.get('/', function(req, res) {
-  res.end('howdy')
+  res.end('howdy', function() {
+    console.log(req.route)
+  })
 })
 
 var server = app.listen(8081, function () {
