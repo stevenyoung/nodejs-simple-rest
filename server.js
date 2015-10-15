4df29e4958230409ced8af7490d1f17b782d8cc1
Steven Young <fivepockets@gmail.com>
2015-10-15 12:16:45 -0700

    adding a path for static files

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var app = express()
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'jade')
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/listUsers', function (req, res) {
   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
